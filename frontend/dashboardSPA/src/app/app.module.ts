a0b0a8725e062ff50811d88eaeb90c8d9def1954
chit <chit.thiri.maung@dksh.com>
2023-06-22 09:49:34 +0800

    draft UI

--- frontend/dashboardSPA/src/app/app.module.ts
+++ frontend/dashboardSPA/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
+import { UserNavigationComponent } from './components/user-navigation/user-navigation.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { DataGraphComponent } from './components/data-graph/data-graph.component';
+import { DataContainerComponent } from './components/data-container/data-container.component';
+import { DataBoxComponent } from './components/data-box/data-box.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DataHeaderComponent } from './components/data-header/data-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavigationComponent,
+    UserNavigationComponent,
+    DataTableComponent,
+    DataGraphComponent,
+    DataContainerComponent,
+    DataBoxComponent,
+    DashboardComponent,
+    DataHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/dashboardSPA/src/app/components/data-header/data-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-header',
+  templateUrl: './data-header.component.html',
+  styleUrls: ['./data-header.component.scss']
+})
+export class DataHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
