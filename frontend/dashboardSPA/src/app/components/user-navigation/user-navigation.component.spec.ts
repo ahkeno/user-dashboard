ef5ab294dd7ea8c32821d4b3f219695708193ab0
chit <chit.thiri.maung@dksh.com>
2023-06-23 10:14:23 +0800

    adding unit test for pages

--- frontend/dashboardSPA/src/app/components/user-navigation/user-navigation.component.spec.ts
+++ frontend/dashboardSPA/src/app/components/user-navigation/user-navigation.component.spec.ts
@@ -1,15 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture,async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import {By} from '@angular/platform-browser';
 import { UserNavigationComponent } from './user-navigation.component';
-
+import { HomepageComponent } from './../../pages/homepage/homepage.component';
 describe('UserNavigationComponent', () => {
   let component: UserNavigationComponent;
   let fixture: ComponentFixture<UserNavigationComponent>;
-
+  let mockRouter:any;
   beforeEach(async () => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [ UserNavigationComponent ]
+      
+      imports: [RouterTestingModule.withRoutes([{ path: 'home', component: HomepageComponent }]),],
+      declarations: [ UserNavigationComponent ],
+      providers: [
+        { provide: Router, useValue: mockRouter},
+      ]
     })
     .compileComponents();
   });
@@ -22,5 +31,18 @@ describe('UserNavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should click user onClickUserDropDown', async(() => {
+    spyOn(component, 'onClickUserDropDown');
+    let button = fixture.debugElement.query(By.css('.user-nav-container'));
+    button.triggerEventHandler('click', null);
+    fixture.whenStable().then(() => {
+      expect(component.onClickUserDropDown).toHaveBeenCalled();
+    });
+  }));
+  it('should navigate', () => {
+    fixture.detectChanges();
+    component.onClickLogout();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['home']);
   });
 });
--- frontend/dashboardSPA/src/app/pages/homepage/homepage.component.spec.ts
+++ frontend/dashboardSPA/src/app/pages/homepage/homepage.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomepageComponent } from './homepage.component';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Router } from '@angular/router';
+import { DashboardComponent } from './../dashboard/dashboard.component';
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
-
+  let mockRouter : any;
   beforeEach(async () => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [ HomepageComponent ]
+      imports: [RouterTestingModule.withRoutes([{ path: 'dashboard', component: DashboardComponent }]),],
+      declarations: [ HomepageComponent ],
+      providers: [
+        { provide: Router, useValue: mockRouter},
+      ]
     })
     .compileComponents();
   });
@@ -28,5 +35,10 @@ describe('HomepageComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h2')?.textContent).toContain('Welcome Home Page');
+  });
+  it('should navigate dashboard', () => {
+    fixture.detectChanges();
+    component.onClickDashboard();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['dashboard']);
   });
 });
